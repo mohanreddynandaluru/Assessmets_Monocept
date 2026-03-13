13838c2e3136c3d601b60224efda881ecf71220b
mohanreddynandaluru <mohanreddy232312@gmail.com>
2026-03-13 23:38:53 +0530

    ArrayOfAccounts

---
+++ ArrayofAccounts/Account.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayofAccounts
+{
+    internal class Account
+    {
+        private string _AccountNumber;
+        private string _AccountName;
+        private double _Balance;
+        private string _AccountType;
+
+        public string AccountNumber
+        {
+            get { return _AccountNumber; }
+        }
+
+        public string AccountName
+        {
+            get { return _AccountName; }
+        }
+
+        public double Balance
+        {
+            get { return _Balance; }
+        }
+
+        public string AccountType
+        {
+            get { return _AccountType; }
+        }
+
+        public Account(string accountNumber, string accountName, double balance, string accountType)
+        {
+            _AccountNumber = accountNumber;
+            _AccountName = accountName;
+            _Balance = balance;
+            _AccountType = accountType;
+        }
+
+        public void Deposit(double amount)
+        {
+            _Balance += amount;
+            Console.WriteLine("Amount Deposited Successfully.");
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (_Balance - amount < 0)
+            {
+                Console.WriteLine("insufficient Balance");
+            }
+            else
+            {
+                _Balance -= amount;
+                Console.WriteLine("Withdrawal Successful.");
+            }
+        }
+    }
+}
--- ArrayofAccounts/Program.cs
+++ ArrayofAccounts/Program.cs
@@ -1,2 +1,164 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace ArrayofAccounts
+{
+    class Program
+    {
+
+        public static string GenerateAccountNummber(Account[] Accounts,int n)
+        {
+            Random rd = new Random();
+
+            string accountNumber;
+
+            while (true)
+            {
+                accountNumber = "IDBI1000" + rd.Next(100000, 999999);
+
+                bool exists = false;
+
+                for(int i=0;i<n; i++)
+                {
+                    if(Accounts[i] != null && Accounts[i].AccountNumber == accountNumber)
+                    {
+                        exists = true;
+                        break;
+                    }
+                }
+
+                if(!exists)
+                {
+                    return accountNumber;
+                }
+            }
+        }
+
+        public static Account FindAccount(Account[] accounts, string accountNumber,int n)
+        {
+            for(int i=0; i<n; i++)
+            {
+                if(accounts[i] != null && accounts[i].AccountNumber == accountNumber)
+                {
+                    return accounts[i];
+                }
+            }
+            return null;
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Account[] accounts = new Account[3];
+            int NoOfAccounts = 0;
+            bool next = false;
+
+            do
+            {
+                Console.WriteLine("\n1. Create Account");
+                Console.WriteLine("2. View Balance");
+                Console.WriteLine("3. Deposit");
+                Console.WriteLine("4. Withdraw");
+                Console.WriteLine("5. Exit");
+
+                Console.WriteLine("Enter your choice: ");
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        if (NoOfAccounts >= 3)
+                        {
+                            Console.WriteLine("Account storage full.");
+                            break;
+                        }
+                        Console.WriteLine("Enter Account Holder Name");
+                        string name = Console.ReadLine();
+
+                        Console.WriteLine("Enter the Type of Account (saving/current)");
+                        string AccountType = Console.ReadLine();
+
+                        if(AccountType.ToLower() != "savings" && AccountType.ToLower() != "current")
+                        {
+                            Console.WriteLine("Invalid Account Type");
+                            break;
+                        }
+
+                        Console.Write("Enter Initial Balance: ");
+                        double bal = Convert.ToDouble(Console.ReadLine());
+
+                        if (bal < 0)
+                        {
+                            Console.WriteLine("Initial balance cannot be negative.");
+                            break;
+                        }
+
+                        string AccountNumber = GenerateAccountNummber(accounts, NoOfAccounts);
+
+                        accounts[NoOfAccounts] = new Account(AccountNumber, name, bal, AccountType);
+
+                        NoOfAccounts++;
+
+                        Console.WriteLine("Account Created Successfully");
+                        Console.WriteLine("Your Account Number is {0}", AccountNumber);
+
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Enter Account Number");
+                        string acc = Console.ReadLine();
+
+                        Account accObj = FindAccount(accounts, acc, NoOfAccounts);
+
+                        if (accObj != null)
+                        {
+                            Console.WriteLine($"Balance: {accObj.Balance:C}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Account not Found");
+                        }
+                        break;
+                    case 3:
+                        Console.Write("Enter Account Number: ");
+                        acc = Console.ReadLine();
+
+                        accObj = FindAccount(accounts, acc, NoOfAccounts );
+
+                        if (accObj != null)
+                        {
+                            Console.Write("Enter Deposit Amount: ");
+                            double amt = Convert.ToDouble(Console.ReadLine());
+                            accObj.Deposit(amt);
+                        }
+                        else
+                            Console.WriteLine("Account not found.");
+
+                        break;
+                    case 4:
+                        Console.Write("Enter Account Number: ");
+                        acc = Console.ReadLine();
+                        accObj = FindAccount(accounts, acc, NoOfAccounts);
+                        if (accObj != null)
+                        {
+                            Console.Write("Enter Withdraw Amount: ");
+                            double amt = Convert.ToDouble(Console.ReadLine());
+                            accObj.Withdraw(amt);
+                        }
+                        else
+                            Console.WriteLine("Account not found.");
+                        break;
+                    case 5:
+                        Console.WriteLine("Exiting...");
+                        next = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Input");
+                        break;
+
+                }
+
+
+
+            } while (!next);
+
+        }
+    }
+}
